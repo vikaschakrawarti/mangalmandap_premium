617df010f10a852238867d8034a808e555062ec9
vikaschakrawarti <130843843+vikaschakrawarti@users.noreply.github.com>
2023-07-01 21:27:48 +0530

    Premium.mangalmandap.com paste
    Premium.mangalmandap.com paste

--- dbConf.php
+++ dbConf.php
@@ -2,9 +2,9 @@
 //error_reporting(0);
 error_reporting( E_ALL );
 define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-$name=define('DB_DATABASE', 'premium_matrimony');
+define('DB_USER', 'mangaztt_premium');
+define('DB_PASSWORD', 'zKDx}hHz136w');
+$name=define('DB_DATABASE', 'mangaztt_premium');
 	$db=mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD);
 	$connection=mysqli_select_db($db,DB_DATABASE); 
 
